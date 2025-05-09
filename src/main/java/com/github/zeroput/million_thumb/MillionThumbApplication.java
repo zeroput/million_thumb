186953cd39e7b8ae219b10ba5e0c65023a3222b4
zeroput <zachary_paris@outlook.com>
2025-05-09 20:01:12 +0800

    init my project step by step

---
+++ src/main/java/com/github/zeroput/million_thumb/MillionThumbApplication.java
@@ -0,0 +1,13 @@
+package com.github.zeroput.million_thumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillionThumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillionThumbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/zeroput/million_thumb/MillionThumbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zeroput.million_thumb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillionThumbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
