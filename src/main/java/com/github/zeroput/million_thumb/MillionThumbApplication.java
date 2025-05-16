8a69b17f619276715d1923cd6d5c236e7e3a2325
zeroput <zachary_paris@outlook.com>
2025-05-16 15:28:13 +0800

    add spring task job to batch update the thumb status

--- src/main/java/com/github/zeroput/million_thumb/MillionThumbApplication.java
+++ src/main/java/com/github/zeroput/million_thumb/MillionThumbApplication.java
@@ -3,9 +3,10 @@ package com.github.zeroput.million_thumb;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
-//@EnableScheduling
+@EnableScheduling // 开启定时任务 否则job不运行
 //@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @SpringBootApplication
 public class MillionThumbApplication {
--- src/main/java/com/github/zeroput/million_thumb/mapper/BlogMapper.java
+++ src/main/java/com/github/zeroput/million_thumb/mapper/BlogMapper.java
@@ -2,6 +2,9 @@ package com.github.zeroput.million_thumb.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.zeroput.million_thumb.model.entity.Blog;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 /**
 * @author Arch
@@ -10,6 +13,9 @@ import com.github.zeroput.million_thumb.model.entity.Blog;
 * @Entity generator.domain.Blog
 */
 public interface BlogMapper extends BaseMapper<Blog> {
+
+    // @@Param("countMap")
+    void batchUpdateBlogThumbCount(@Param("countMap") Map<Long, Long> countMap);
 
 }
 
